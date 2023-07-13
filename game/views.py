bcd6c87d43948efcaf974b613b8867bb6b775e07
Jiang-liran <747329069@qq.com>
2023-07-13 16:52:02 +0800

    write my first page

--- game/views.py
+++ game/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def index(request):
+    line1 = '<h1 style="text-align:center">术士之战</h1>'
+    line2 = '<img src="https://b0.bdstatic.com/ugc/bIZ_283CNAtEbtxtzqLvbQ350c9c933efe9b44bc9587b95c93c389.jpg@h_1280">'
+    return HttpResponse(line1 + line2)
+
