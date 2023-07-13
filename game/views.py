d48d36f44cbb737d6f18c7354015bff97c557a50
Jiang-liran <747329069@qq.com>
2023-07-13 18:20:48 +0800

    create project structure

--- game/views.py
+++
@@ -1,7 +0,0 @@
-from django.http import HttpResponse
-
-def index(request):
-    line1 = '<h1 style="text-align:center">术士之战</h1>'
-    line2 = '<img src="https://b0.bdstatic.com/ugc/bIZ_283CNAtEbtxtzqLvbQ350c9c933efe9b44bc9587b95c93c389.jpg@h_1280">'
-    return HttpResponse(line1 + line2)
-
